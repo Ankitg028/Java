c23f45fbf240b93028a06bd5daad60fdf19947ee
Ankitg028 <kavyg028@gmail.com>
2021-07-01 12:01:54 +0530

    train commit

---
+++ Abstraction & Packages & Exception Handling/Abstract/2/main.java
@@ -0,0 +1,10 @@
+/*
+ * Create an abstract class Compartment to represent a rail coach. Provide an abstract 
+ * function notice in this class. Derive FirstClass, Ladies, General, Luggage classes 
+ * from the compartment class. Override the notice function in each of them to print notice 
+ * suitable to the type of the compartment.
+ * Create a class TestCompartment . Write main function to do the following:
+ * Declare an array of Compartment of size 10.
+ * Create a compartment of a type as decided by a randomly generated integer in the range 1 to 4.
+ * Check the polymorphic behavior of the notice method.
+ * */
